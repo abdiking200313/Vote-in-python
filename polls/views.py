a2f6ef8334753ee9f0d5013601ed44f13bcd379f
00310843 <00310843@rijnijssel.nl>
2025-02-16 21:54:12 +0100

    voting pages

--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. Youre at the polls index")
+def detail(request, question_id):
+    return HttpResponse("Youre looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "Youre looking at the results of questions %s."
+    return HttpResponse( response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Youre voting on question %s." % question_id)
 
 # Create your views here.
